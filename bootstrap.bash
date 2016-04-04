5952f666fcea8dfb19e939ad692a5e6a725080f5
aegypius <innercircle@aegypius.com>
2016-04-04 10:50:20 +0200

    Use sudo to perform install of git if necessary, add fedora support

--- bootstrap.bash
+++ bootstrap.bash
@@ -4,13 +4,18 @@
 #
 #    bash <(curl -sL https://raw.githubusercontent.com/aegypius/dotfiles/master/bootstrap.bash)
 #
+set -eo pipefail
 
 # Ensure git is installed if not install it (support debian or gentoo for now)
 ( \
-  (type git > /dev/null 2>&1) || \
-  (type apt-get > /dev/null 2>&1 && apt-get install -y --no-install-recommends git-core) || \
-  (type emerge > /dev/null 2>&1 && emerge git) \
+  type git > /dev/null 2>&1 || \
+  (type apt-get > /dev/null 2>&1 && sudo apt-get install -y --no-install-recommends git-core) || \
+  (type dnf > /dev/null 2>&1 && sudo dnf install -y git) || \
+  (type emerge > /dev/null 2>&1 && sudo emerge git) || \
+  (echo "Failed to install git" && false) \
 )
+
+set +eo pipefail
 
 # Clones homeshick repository
 (
